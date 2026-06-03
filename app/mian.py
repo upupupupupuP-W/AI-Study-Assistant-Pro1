28e4afdeaf104b1e05dde035ca68d21c30e03f91
xtw40412-netizen <xtw40412@gemail.com>
2026-06-03 19:39:06 +0800

    Fisish Day3 if elif else

--- app/mian.py
+++ app/mian.py
@@ -1,5 +1,6 @@
 
 print("Hello AI Study Assistant Pro")
+# DAY2
 # 1.认识变量
 # name="张三"
 # age=20
@@ -50,10 +51,65 @@ print("Hello AI Study Assistant Pro")
 
 
 
-# 5.第一个模块，只能成绩分析器V1.0
-name=input("请输入学生姓名：")
+# # 5.第一个模块，只能成绩分析器V1.0
+# name=input("请输入学生姓名：")
 
-score=int(input("请输入学生成绩："))
+# score=int(input("请输入学生成绩："))
+# if score>=90:
+#     print("等级：A")
+# elif score>=80:
+#     print("等级：B")
+# elif score>=70:
+#     print("等级：C")
+# elif score>=60:
+#     print("等级:D")   
+# else:
+#     print("等级:E")    
+
+
+
+
+
+
+
+
+
+# # DAY3
+# # 1.if+else
+# age=int(input("请输入年龄："))
+# if age>=18:
+#     print("你已经成年了")
+# else:
+#     print("还未成年，要好好长大。")
+
+
+
+
+# 2.理解elif
+# score=int(input("请输入成绩："))
+# if score>=90:
+#     print("A")
+# elif score>=80:
+#     print("B")
+# elif score>=70:
+#     print("C")
+# elif score>=60:
+#     print("D")
+# else:
+#     print("你不及格啦，要好好努力学习哦。")
+
+
+
+
+
+# 3.项目练习
+name=input("请输入姓名：")
+score=int(input("请输入成绩："))
+
+print("\n=====成绩分析=====")
+print("姓名：",name)
+print("成绩：",score)
+
 if score>=90:
     print("等级：A")
 elif score>=80:
@@ -61,6 +117,6 @@ elif score>=80:
 elif score>=70:
     print("等级：C")
 elif score>=60:
-    print("等级:D")   
+    print("等级：D")
 else:
-    print("等级:E")    
+    print("不及格，需要好好学习了。")
